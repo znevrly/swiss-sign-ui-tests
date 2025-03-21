ce8c4bce10ebd28fd0ff77c74dd08b42ed96f9f8
znevrly <znevrly@gmail.com>
2025-03-21 13:50:43 +0100

    add screenshots on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /* Capture screenshot on test failure */
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
